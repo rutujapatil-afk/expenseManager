0645d05c89bdba9e2f597da93be7b73420e161f1
Rutuja Patil <rutujakpatil2003@gmail.com>
2024-11-17 14:31:06 +0530

    updated models/policy_suggestions.py

--- models/policy_suggestions.py
+++ models/policy_suggestions.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report
+
 
 # Load Datasets
 @st.cache_data
