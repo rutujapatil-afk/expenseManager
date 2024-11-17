636a44e4f42fda87bd90ae00671236902200f07c
Rutuja Patil <rutujakpatil2003@gmail.com>
2024-11-18 01:36:07 +0530

    updated models/policy_suggestions.py

--- models/policy_suggestions.py
+++ models/policy_suggestions.py
@@ -1,3 +1,5 @@
+st.set_page_config(page_title="Investment Dashboard", layout="wide")
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -7,8 +9,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
-
-st.set_page_config(page_title="Investment Dashboard", layout="wide")
 
 # Load Datasets
 @st.cache_data
